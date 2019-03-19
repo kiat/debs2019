b87baf1be1988e9cb7e234627366f1e51902c9e4
samba693 <sambasivarao693@gmail.com>
2019-03-19 14:58:46 -0400

    Plotting both outliers and objects

--- src/object-net/data.py
+++ src/object-net/data.py
@@ -16,8 +16,10 @@ import plotly.graph_objs as go
 def remove_outliers(file_path):
     # return the list of dataframes
     dataframe_lists = []
+    outliers = []
     # Creating the dataframe and selecting the required columns
     for i in range(3):
+        temp_out = pd.DataFrame()
         df = pd.read_csv(file_path, usecols=[1,2,3,4], skiprows=i*72000, nrows = 72000, names=["lz","X","Y","Z"])
         df['radiusSquare'] = df['X']*df['X']+df['Y']*df['Y']+df['Z']*df['Z']
         df['radius'] = np.sqrt(df['radiusSquare']).round(1)
@@ -25,12 +27,15 @@ def remove_outliers(file_path):
         for j in range(64):
             maxfreq = df[(df['lz']==j) & (df['radius']!=0)]['freq'].max()
             while maxfreq>75:
+                temp_out =  temp_out.append(df.loc[(df['lz']==j) & (df['freq']==maxfreq)],ignore_index=True)
                 df.drop(df[(df['lz']==j) & (df['freq']==maxfreq)].index, inplace=True)
                 maxfreq = df[(df['lz']==j) & (df['radius']!=0)]['freq'].max()
+            temp_out =  temp_out.append(df.loc[(df['lz']==j) & (df['radius']==0)],ignore_index=True)
             df.drop(df[(df['lz']==j) & (df['radius']==0)].index, inplace=True)
+        outliers.append(temp_out.iloc[:,1:4])
         dataframe_lists.append(df.iloc[:,1:4])
 
-    return dataframe_lists
+    return outliers, dataframe_lists
 
 # cluster the data points and get the results
 def cluster(dataframes):
@@ -76,6 +81,10 @@ def visualize_base(data, type_of_vis, count=1):
         plt.show()
     
     else:
-        trace = go.Scatter3d(x=x,y=y,z=z, mode='markers', marker=dict(color='rgb(0, 0, 0)',size=2,opacity=0.6))
+        if 'labels' in data:
+            labels = data['labels'].tolist()
+            trace = go.Scatter3d(x=x,y=y,z=z, mode='markers', marker=dict(color=labels,size=2,opacity=0.6))
+        else:
+            trace = go.Scatter3d(x=x,y=y,z=z, mode='markers', marker=dict(color='red',size=2,opacity=0.6))
         data=[trace]
-        plot(data,filename='../visuals/{}_{}.html'.format('vis',count), auto_open=False, show_link=False)        
+        plot(data,filename='../visuals/{}_{}.html'.format('vis',count), auto_open=False, show_link=False)
