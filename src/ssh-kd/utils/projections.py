a68c524e1992bdde60d1f5903cbcd665d7a896de
Harshad <harshadreddy16@gmail.com>
2019-04-20 04:08:04 -0400

    Projections for point cloud

---
+++ src/ssh-kd/utils/projections.py
@@ -0,0 +1,16 @@
+def normalize(df):
+    df['_x'] = (df['_x']-df['_x'].min())/(df['_x'].max()-df['_x'].min())
+    df['_y'] = (df['_y']-df['_y'].min())/(df['_y'].max()-df['_y'].min())
+    return df
+
+
+def prespective_project(df, d):
+    df['_x'] = ((df['X']/abs(df['Z']))*d)
+    df['_y'] = ((df['Y']/abs(df['Z']))*d)
+    return df
+
+
+def cabin_projection(df, alpha):
+    df['_x'] = (df['X'] + (0.2 * df['Z']) * np.cos(np.deg2rad(alpha)))
+    df['_y'] = (df['Y'] + (0.2 * df['Z']) * np.sin(np.deg2rad(alpha)))
+    return df
